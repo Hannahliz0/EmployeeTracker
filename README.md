477c5ca256416cb22e67956c5a9814145452c5dc
Gianna DAconti <themeanmememan@gmail.com>
2024-10-14 21:00:08 -0400

    Full app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SQLTracker
+# SQL Tracker
+
+This application allows you to build an employee Tracker Table & edit roles of Employees.
+
+# Demo Video Link
+
+https://app.screencastify.com/v3/watch/O4Te0IYxUNWzclg4PLvG
+
+# Repo Link
+
+https://github.com/CodeCourtAce/SQLTracker.git
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+-- Insert departments
+INSERT INTO department (name) VALUES 
+\c SQL_Tracker_db
+
+('HR'), 
+('Engineering'), 
+('Sales'),
+('Legal'),
+('Marketing'),
+('Finance'),
+('IT')
+ON CONFLICT (name) DO NOTHING;
+
+-- Insert roles
+INSERT INTO role (title, salary, department_id) VALUES 
+('HR Manager', 75000, 1),
+('Engineer', 90000, 2),
+('Salesperson', 60000, 3),
+('Lawyer', 120000, 4),
+('Marketing Manager', 80000, 5),
+('Accountant', 70000, 6),
+('IT Manager', 85000, 7)
+ON CONFLICT (title) DO NOTHING;
+
+-- Insert employees
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Tony', 'Stark', 1, NULL),
+('Remy', 'LeBeau', 2, 1),
+('Wanda', 'Maximoff', 3, 1);
+('Peter', 'Parker', 4, 1),
+('Mike', 'Wazowski', 5, 1),
+('Indiana', 'Jones', 6, 1),
+('Kelly', 'Garrett', 7, 1)
+ON CONFLICT (first_name, last_name) DO NOTHING;
